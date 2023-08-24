9e5784bb59cfe1e3e63e7e3a6a03dc473b2e698a
ankitdas13 <ankit.das@razorpay.com>
2023-08-25 02:39:35 +0530

    add typewriter effect

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,15 +4,15 @@ export default function Footer() {
             <div className="footer-content flex gap-4">
                 <div className="contact-info">
                     <h3 className='text-2xl font-bold text-center mb-4'>CHAT</h3>
-                    <p className='text-[#808CAD] text-lg'>Telegram</p>
+                    <a className='text-[#808CAD] text-lg' href="https://t.me/Ankit10204">Telegram</a>
                 </div>
                 <div className="contact-info">
                     <h3 className='text-2xl font-bold text-center mb-4'>EMAIL</h3>
-                    <p className='text-[#808CAD] text-lg'>ankitdas10204@gmail.com</p>
+                    <a className='text-[#808CAD] text-lg' href="mailto:ankitdas10204@gmail.com">ankitdas10204@gmail.com</a>
                 </div>
             </div>
             <div className="copyright m-10">
-                <p className="text-[#808CAD] text-lg text-center">Copyright &copy; Developed by Ankit Das. All right reserved.</p>
+                <p className="text-[#808CAD] text-lg text-center">Copyright &copy; Developed by <a className="text-blue-400" href="https://ankitdas.dev/">Ankit Das</a>. All right reserved.</p>
             </div>
         </footer>
     )
--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,11 +1,32 @@
 import profile from "@/public/profile.jpg"
+import { useEffect } from "react";
+import Typewriter from 'typewriter-effect/dist/core';
 
 export default function Hero() {
+     useEffect(()=>{
+        var app = document.getElementById('hero-name');
+        var typewriter = new Typewriter(app, {
+            loop: true,
+            delay: 75,
+        });
+
+        typewriter
+        .pauseFor(1500)
+        .typeString('Ankit Das')
+        .pauseFor(3500)
+        .deleteChars(15)
+        .typeString("<span style='color:#00e7a0;'>a</span> FullStack Developer")
+        .pauseFor(5000)
+        .start(); 
+     })
     return (
         <>
             <div className="im-content basis-3/5 px-10">
-                <h2 className='text-green-400 text-5xl mt-10 font-medium mb-4'>I'm Ankit Das,</h2>
-                <p className='text-[#808dad] text-xl'>Greetings! I'm Ankit Das, a skilled Full Stack Engineer with 5 years of experience in designing and developing scalable web applications. My passion for innovation drives me to create solutions that leave a lasting impact.</p>
+                <h2 className='text-green-400 text-3xl mt-10 font-medium'>Hello there,</h2>
+                <h1 className='text-green-400 text-4xl font-medium mb-4 font-extrabold'>I'm <span className="text-white" id="hero-name"></span></h1>
+                <p className='text-[#808dad] text-xl'>I'm Ankit Das, a skilled Full Stack Developer with 5 years of experience in developing scalable web applications. My passion for innovation drives me to create solutions that leave a lasting impact.</p>
+                
+               
                 <div className="click-sec">
                     <button className="clickme color-gred"  onClick={(e) => {e.preventDefault(); window.location.href = '/files/resume.pdf';}}>
                         Download CV
