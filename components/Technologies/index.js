6c2edbbb20ca6a707992be18bfcf220e0b893951
ankitdas13 <ankit.das@razorpay.com>
2023-08-27 03:29:56 +0530

    added skill proficiency

--- components/Technologies/index.js
+++ components/Technologies/index.js
@@ -1,26 +1,31 @@
 import { Skills } from "./skills"
 
-export default function Technologies(){
+export default function Technologies() {
     return (
         <>
-        <hr />
-        <div className="im-content basis-1/2 px-10 pb-5">
-            <h2 className='text-green-400 text-4xl mt-10 font-bold mb-2'>Skills</h2>
-            <p className='text-[#808dad] text-xl'>I have extensive experience working with a variety of technologies as a developer. I've developed and maintained multiple projects using these technologies, and I'm always eager to learn more.</p>
-            <div className="grid grid-cols-1 md:grid-cols-3 mt-8 gap-10">
-                {Skills.map((Skill,key) => (
-                    <div key={key} className="mt-5 flex flex-row mt-16">
-                        <div className='mb-10'><Skill.Component size={38} /></div>
+            <hr />
+            <div className="im-content basis-1/2 px-10 pb-5">
+                <h2 className='text-green-400 text-4xl mt-10 font-bold mb-2'>Skills</h2>
+                <p className='text-[#808dad] text-xl'>I have extensive experience working with a variety of technologies as a developer. I've developed and maintained multiple projects using these technologies, and I'm always eager to learn more.</p>
+                <div className="grid grid-cols-1 md:grid-cols-3 mt-8 gap-10">
+                    {Skills.map((Skill, key) => (
                         <div>
-                            <div className='text-3xl font-bold px-4 mb-3'>{Skill.slug.toLocaleUpperCase()}</div>
-                            <div className='text-base px-4'>
-                                <p>{Skill.Description}</p>
+                            <div key={key} className="mt-5 mt-16 flex flex-row bg-white border border-gray-200 rounded-t-lg shadow dark:bg-gray-800 dark:border-gray-700 px-2 pt-5 h-full">
+                                <div className='mb-10'><Skill.Component size={38} /></div>
+                                <div>
+                                    <div className='text-3xl font-bold px-4 mb-3'>{Skill.slug.toLocaleUpperCase()}</div>
+                                    <div className='text-base px-4'>
+                                        <p>{Skill.Description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="bg-slate-900 w-full h-1 rounded-b-lg relative" style={{ 'borderRadius': '0 0 0 25px;', 'top': '-3px' }}>
+                                <div className="bg-green-400 h-1" style={{ 'width': `${Skill.proficiency}`, 'borderRadius': '0 0 0 25px' }}></div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
         </>
     )
 }
--- components/Technologies/skills.js
+++ components/Technologies/skills.js
@@ -1,67 +1,111 @@
-import { SiGit, SiDart, SiCss3, SiMysql, SiAmazonaws, SiTsnode, SiPostgresql, SiMongodb } from "react-icons/si";
+import { SiGit, SiDart, SiCss3, SiMysql, SiAmazonaws, SiTsnode, SiPostgresql, SiMongodb,SiExpress,SiEthereum } from "react-icons/si";
 import { DiHtml5, DiReact, DiJqueryLogo } from "react-icons/di";
-import { BiLogoPostgresql, BiLogoPhp, BiLogoJavascript, BiLogoGithub, BiSolidMoon } from "react-icons/bi"
+import { BiLogoPostgresql, BiLogoPhp, BiLogoJavascript, BiLogoGithub, BiSolidMoon, BiLogoGraphql } from "react-icons/bi"
 import { FaFacebookSquare, FaLinkedin, FaNodeJs } from "react-icons/fa";
+import { VscJson } from "react-icons/vsc"
+import { TbBrandDocker } from "react-icons/tb"
 
 export const Skills = [
     {
         slug: 'php',
         Component: BiLogoPhp,
-        Description: 'Php, Laravel & Codeigniter frameworks'
+        Description: 'Php, Laravel & Codeigniter frameworks',
+        proficiency: '70%'
     },
     {
         slug: 'node',
         Component: FaNodeJs,
-        Description: 'Dynamic APIs and server-side applications development.'
+        Description: 'Dynamic APIs and server-side applications development.',
+        proficiency: '70%'
+    },
+    {
+        slug: 'express js',
+        Component: SiExpress,
+        Description: 'Skilled in building applications with Express.js framework.',
+        proficiency: '50%'
     },
     {
         slug: 'type-script',
         Component: SiTsnode,
-        Description: 'Strong typing for scalable app development.'
+        Description: 'Strong typing for scalable app development.',
+        proficiency: '50%'
     },
     {
         slug: 'react',
         Component: DiReact,
-        Description: 'Building dynamic user interfaces and components.'
+        Description: 'Building dynamic user interfaces and components.',
+        proficiency: '80%'
     },
     {
         slug: 'js',
         Component: BiLogoJavascript,
-        Description: 'Simplifying DOM manipulation and interactive web development.'
-    },
-    {
-        slug: 'aws',
-        Component: SiAmazonaws,
-        Description: 'Utilizing S3 and EC2 for scalable infrastructure.'
-    },
-    {
-        slug: 'git',
-        Component: SiGit,
-        Description: 'Version control and collaborative code management platform.'
+        Description: 'Simplifying DOM manipulation and interactive web development.',
+        proficiency: '90%'
     },
     {
         slug: 'html',
         Component: DiHtml5,
-        Description: 'Static webpages and portfolio projects'
+        Description: 'Static webpages and portfolio projects',
+        proficiency: '90%'
     },
     {
         slug: 'css',
         Component: SiCss3,
-        Description: 'Styling of my webpages'
+        Description: 'Styling of my webpages',
+        proficiency: '60%'
+    },
+    {
+        slug: 'solidity',
+        Component: SiEthereum,
+        Description: 'Knowledgeable in developing smart contracts using Solidity.',
+        proficiency: '40%'
     },
     {
         slug: 'mysql',
         Component: SiMysql,
-        Description: 'Storing client and user data'
+        Description: 'Storing client and user data',
+        proficiency: '60%'
     },
     {
         slug: 'postgres',
         Component: SiPostgresql,
-        Description: 'Storing data with reliable relational database management.'
+        Description: 'Storing data with reliable relational database management.',
+        proficiency: '60%'
     },
     {
         slug: 'mongo db',
         Component: SiMongodb,
-        Description: 'NoSQL database for flexible and scalable data storage.'
+        Description: 'NoSQL database for flexible and scalable data storage.',
+        proficiency: '50%'
     },
+    {
+        slug: 'graph ql',
+        Component: BiLogoGraphql,
+        Description: 'Experienced in crafting effective, team-friendly GraphQL APIs.',
+        proficiency: '50%'
+    },
+    {
+        slug: 'rest api',
+        Component: VscJson,
+        Description: 'Experienced in designing and developing RESTful APIs.',
+        proficiency: '90%'
+    },
+    {
+        slug: 'git',
+        Component: SiGit,
+        Description: 'Version control and collaborative code management platform.',
+        proficiency: '90%'
+    },
+    {
+        slug: 'docker',
+        Component: TbBrandDocker,
+        Description: 'Familiar with containerization using Docker technology.',
+        proficiency: '30%'
+    },
+    {
+        slug: 'aws',
+        Component: SiAmazonaws,
+        Description: 'Utilizing S3 and EC2 for scalable infrastructure.',
+        proficiency: '30%'
+    }
 ]
